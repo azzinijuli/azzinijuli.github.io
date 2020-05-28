bb52606a9a807e047211a9c706e900f4be46ac36
Julieta Azzini <juliazzini1@gmail.com>
2020-05-28 20:18:46 -0300

    modified hamburger menu

--- JS/index.js
+++ JS/index.js
@@ -2,9 +2,9 @@ const menu = document.querySelector(".js-hamburgerMenu");
 const menuItems = document.querySelector(".js-navMenu");
 
 menu.onclick = function () {
-    if (menuItems.style.display === "block") {
+    if (menuItems.style.display === "inline-flex") {
         menuItems.style.display = "none";
     } else {
-        menuItems.style.display = "block";
+        menuItems.style.display = "inline-flex";
     }
 }
