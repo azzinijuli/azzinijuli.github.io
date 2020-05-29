e5d91e82fcfafda9dc8fe761c145257f50a5ab42
Julieta Azzini <juliazzini1@gmail.com>
2020-05-28 22:50:41 -0300

    fixed error in hamburger menu

--- JS/index.js
+++ JS/index.js
@@ -2,9 +2,5 @@ const menu = document.querySelector(".js-hamburgerMenu");
 const menuItems = document.querySelector(".js-navMenu");
 
 menu.onclick = function () {
-    if (menuItems.style.display === "inline-flex") {
-        menuItems.style.display = "none";
-    } else {
-        menuItems.style.display = "inline-flex";
-    }
+    menuItems.classList.toggle('show');
 }
