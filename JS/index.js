e2a1e133bc50989838230733ef98a7e2b7fdf16d
Julieta Azzini <juliazzini1@gmail.com>
2020-06-16 16:09:01 -0300

    added carousel

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,15 @@
 const menu = document.querySelector(".js-hamburgerMenu");
 const menuItems = document.querySelector(".js-navMenu");
 
+$(document).ready(function(){
+    $('.series-carousel').slick({
+        variableWidth: true,
+        infinite: true,
+        arrows: true,
+        accesibility: true,
+    });
+});
+
 menu.onclick = function () {
     menuItems.classList.toggle('show');
 }
